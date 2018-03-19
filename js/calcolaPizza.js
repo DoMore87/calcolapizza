e7c4f9792fda36af832f33408b227029175a59be
Domenico Moreschini <tech593@yoox.com>
2018-03-19 12:37:15 +0100

    Rename to undestand

--- js/calcolaPizza.js
+++ js/calcolaPizza.js
@@ -1,71 +1,53 @@
 $(window).load(function() {
     function a() {
-        var a = 0
-          , r = parseFloat($("#panielli").val().replace(",", "."));
-        isNaN(r) || r < 1 || r % 1 != 0 ? ($("#panielli").parent().addClass("errato"),
-        a = 1) : $("#panielli").parent().removeClass("errato");
-        var e = parseFloat($("#peso").val().replace(",", "."));
-        isNaN(e) || e <= 0 ? ($("#peso").parent().addClass("errato"),
-        a = 1) : $("#peso").parent().removeClass("errato");
-        var t = parseFloat($("#idro").val().replace(",", "."));
-        isNaN(t) || t < 50 || t > 100 ? ($("#idro").parent().addClass("errato"),
-        a = 1) : $("#idro").parent().removeClass("errato");
-        var l = parseFloat($("#salepl").val().replace(",", "."));
-        isNaN(l) || l < 0 || l > 70 ? ($("#salepl").parent().addClass("errato"),
-        a = 1) : $("#salepl").parent().removeClass("errato");
-        var i = parseFloat($("#liev").val().replace(",", "."));
-        isNaN(i) || i < 3 || i > 96 ? ($("#liev").parent().addClass("errato"),
-        a = 1) : $("#liev").parent().removeClass("errato");
-        var s = parseFloat($("#frigo").val().replace(",", "."));
-        $("#frigo").parent().attr("title", "Inserire un numero non negativo"),
-        (isNaN(s) || s < 0 || s > i - 1) && 0 != s ? (s > i - 1 && $("#frigo").parent().attr("title", "Non superare il totale ore diminuito di 1"),
-        $("#frigo").parent().addClass("errato"),
-        a = 1) : $("#frigo").parent().removeClass("errato");
-        var o = $("#teglia").is(":checked");
-        console.log("teglia = " + o);
-        var p = parseFloat($("#gradi").val().replace(",", "."));
-        isNaN(p) || p < 15 || p > 35 ? ($("#gradi").parent().addClass("errato"),
-        a = 1) : $("#gradi").parent().removeClass("errato");
-        var n = p * (1 - .25 * o)
-          , d = parseFloat($("#grassipl").val().replace(",", "."));
-        isNaN(d) || d < 0 || d > 150 ? ($("#grassipl").parent().addClass("errato"),
-        a = 1) : $("#grassipl").parent().removeClass("errato");
-        var v = parseFloat($("#pdrp").val().replace(",", "."));
+        var a = 0;
+        var panielli = parseFloat($("#panielli").val().replace(",", "."));   // integer, > 0        
+        var pesoPanielli = parseFloat($("#peso").val().replace(",", "."));    // > 0        
+        var idro = parseFloat($("#idro").val().replace(",", "."));    // 50 < x < 100
+        var salePerLitro = parseFloat($("#salepl").val().replace(",", "."));   //  0 < x < 70        
+        var oreLievitazione = parseFloat($("#liev").val().replace(",", "."));  //  3 < x < 96                
+        var inFrigo = parseFloat($("#frigo").val().replace(",", "."));   //  0 < x < oreLievitazione - 1
+        var inTeglia = $("#teglia").is(":checked"); // bool
+        var gradi = parseFloat($("#gradi").val().replace(",", "."));   //  15 < x < 35
+        var gradiNormalizzati = gradi * (1 - .25 * inTeglia)
+        var grassiPerLitro = parseFloat($("#grassipl").val().replace(",", ".")); //  0 < x < 150
+
+        var pastaRiporto = parseFloat($("#pdrp").val().replace(",", "."));
         switch (parseFloat($('input[name="pdrt"]:checked').val())) {
         case 1:
-            g = 1 / 300;
+            coeffPdr = 1 / 300;   // 0,003333333
             break;
         case 2:
-            g = .005;
+            coeffPdr = .005;
             break;
         case 3:
-            var g = .01
+            var coeffPdr = .01
         }
-        var c = i - 9 * s / 10
-          , N = 81.4206918743428 + 78.3939060802556 * Math.log(i)
-          , m = 10 * Math.round(N / 10)
-          , h = 2250 * (1 + l / 200) * (1 + d / 300) / ((4.2 * t - 80 - .0305 * t * t) * Math.pow(n, 2.5) * Math.pow(c, 1.2))
-          , u = r * e
-          , C = t * (l + d) + 1e3 * (t + 100)
-          , F = (1e5 * h * 100 / (1e5 * h + C * g)).toString().match(/^\d+(?:\.\d{0,2})?/);
-        1 == a ? $("#pdrp").parent().attr("title", "Correggere i dati precedenti") : $("#pdrp").parent().attr("title", "Inserire un numero tra 0 e " + F),
-        isNaN(v) || v < 0 || v > F ? ($("#pdrp").parent().addClass("errato"),
-        a = 1) : $("#pdrp").parent().removeClass("errato");
-        var f = u * v / 100
-          , k = 1e5 * (u - f) / C
-          , S = (1e3 * t * (u - f) / C).toFixed(0)
-          , x = (l * t * (u - f) / C).toFixed(0)
-          , w = (d * t * (u - f) / C).toFixed(0)
-          , b = (k * h - g * f).toFixed(2);
-        k = k.toFixed(0),
-        f = f.toFixed(0),
-        isNaN(m) || 1 == a ? $("#forza").html("-") : $("#forza").html(m.toString()),
-        isNaN(k) || 1 == a ? $("#farina").html("-") : $("#farina").html(k.toString() + " g"),
-        isNaN(S) || 1 == a ? $("#aqua").html("-") : $("#aqua").html(S.toString() + " g"),
-        isNaN(x) || 1 == a ? $("#sale").html("-") : $("#sale").html(x.toString() + " g"),
-        isNaN(w) || 1 == a ? $("#grassi").html("-") : $("#grassi").html(w.toString() + " g"),
-        isNaN(f) || 1 == a ? $("#pdr").html("-") : $("#pdr").html(f.toString() + " g"),
-        isNaN(b) || 1 == a ? $("#lievito").html("-") : $("#lievito").html(b.toString().replace(".", ",") + " g")
+        var oreNormalizzate = oreLievitazione - 9 * inFrigo / 10
+          , coeffForza = 81.4206918743428 + 78.3939060802556 * Math.log(oreLievitazione)  // not used
+          , forzaConsigliata = 10 * Math.round(coeffForza / 10) // not used
+          , pesoTot = panielli * pesoPanielli
+          , coeff = idro * (salePerLitro + grassiPerLitro) + 1e3 * (idro + 100)
+
+          , h = 2250 * (1 + salePerLitro / 200) * (1 + grassiPerLitro / 300) / ((4.2 * idro - 80 - .0305 * idro * idro) * Math.pow(gradiNormalizzati, 2.5) * Math.pow(oreNormalizzate, 1.2))
+          , F = (1e5 * h * 100 / (1e5 * h + coeff * coeffPdr)).toString().match(/^\d+(?:\.\d{0,2})?/);
+        1 == a ? $("#pdrp").parent().attr("title", "Correggere i dati precedenti") : $("#pdrp").parent().attr("title", "Inserire un numero tra 0 e " + F), isNaN(pastaRiporto) || pastaRiporto < 0 || pastaRiporto > F ? ($("#pdrp").parent().addClass("errato"), a = 1) : $("#pdrp").parent().removeClass("errato");
+        
+        var pdrRes = pesoTot * pastaRiporto / 100
+          , farinaRes = 1e5 * (pesoTot - pdrRes) / coeff
+          , acquaRes = (1e3 * idro * (pesoTot - pdrRes) / coeff).toFixed(0)
+          , saleRes = (salePerLitro * idro * (pesoTot - pdrRes) / coeff).toFixed(0)
+          , grassiRes = (grassiPerLitro * idro * (pesoTot - pdrRes) / coeff).toFixed(0)
+          , lievitoRes = (farinaRes * h - coeffPdr * pdrRes).toFixed(2);
+        farinaRes = farinaRes.toFixed(0),
+        pdrRes = pdrRes.toFixed(0),
+        isNaN(forzaConsigliata) || 1 == a ? $("#forza").html("-") : $("#forza").html(forzaConsigliata.toString()),  // not used
+        isNaN(farinaRes) || 1 == a ? $("#farina").html("-") : $("#farina").html(farinaRes.toString() + " g"),
+        isNaN(acquaRes) || 1 == a ? $("#aqua").html("-") : $("#aqua").html(acquaRes.toString() + " g"),
+        isNaN(saleRes) || 1 == a ? $("#sale").html("-") : $("#sale").html(saleRes.toString() + " g"),
+        isNaN(grassiRes) || 1 == a ? $("#grassi").html("-") : $("#grassi").html(grassiRes.toString() + " g"),
+        isNaN(pdrRes) || 1 == a ? $("#pdr").html("-") : $("#pdr").html(pdrRes.toString() + " g"),
+        isNaN(lievitoRes) || 1 == a ? $("#lievito").html("-") : $("#lievito").html(lievitoRes.toString().replace(".", ",") + " g")
     }
     $("body").vegas({
         slides: [{
