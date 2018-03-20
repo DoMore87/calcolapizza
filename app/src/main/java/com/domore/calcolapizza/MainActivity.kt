e59daa05a41c00eda05ef73d43fd3f8da2b7c860
Domenico Moreschini <tech593@yoox.com>
2018-03-20 12:53:21 +0100

    First draft of UI

--- app/src/main/java/com/domore/calcolapizza/MainActivity.kt
+++ app/src/main/java/com/domore/calcolapizza/MainActivity.kt
@@ -2,11 +2,21 @@ package com.domore.calcolapizza
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.pdr_types)
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.pdr_array, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
