5d786192e7843c8179f53066e47c1274f434011c
Domenico Moreschini <tech593@yoox.com>
2018-03-20 16:33:51 +0100

    Use kotlin style

--- app/src/main/java/com/domore/calcolapizza/MainActivity.kt
+++ app/src/main/java/com/domore/calcolapizza/MainActivity.kt
@@ -3,7 +3,7 @@ package com.domore.calcolapizza
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 
@@ -13,10 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinner = findViewById<Spinner>(R.id.pdr_types)
         val adapter = ArrayAdapter.createFromResource(this,
                 R.array.pdr_array, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
+        pdr_types.adapter = adapter
     }
 }
