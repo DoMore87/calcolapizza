7be7375f61d9b19d9fc1214ef46341179fec4e34
Domenico Moreschini <tech593@yoox.com>
2018-03-28 18:00:31 +0200

    Add stupid implementation of button click

--- app/src/main/java/com/domore/calcolapizza/MainActivity.kt
+++ app/src/main/java/com/domore/calcolapizza/MainActivity.kt
@@ -2,17 +2,22 @@ package com.domore.calcolapizza
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(view: View?) {
+        Toast.makeText(this, "Test message", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn_calcola.setOnClickListener(this)
         val adapter = ArrayAdapter.createFromResource(this,
                 R.array.pdr_array, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
