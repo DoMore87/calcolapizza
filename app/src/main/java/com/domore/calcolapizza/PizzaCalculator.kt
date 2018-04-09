03434ca310fc298dbd4944bc80cf103a3c12d839
Domenico Moreschini <tech593@yoox.com>
2018-04-09 10:45:20 +0200

    Add result class and empty calculator

---
+++ app/src/main/java/com/domore/calcolapizza/PizzaCalculator.kt
@@ -0,0 +1,11 @@
+package com.domore.calcolapizza
+
+object PizzaCalculator {
+
+    fun Calculate(input: InputData) : ResultData {
+
+
+        return ResultData(0F,0F,0F,0F,0F,0F)
+    }
+
+}
---
+++ app/src/main/java/com/domore/calcolapizza/ResultData.kt
@@ -0,0 +1,8 @@
+package com.domore.calcolapizza
+
+data class ResultData(val acqua: Float,
+                      val farina: Float,
+                      val sale: Float,
+                      val grassi: Float,
+                      val pdr: Float,
+                      val lievito: Float)
