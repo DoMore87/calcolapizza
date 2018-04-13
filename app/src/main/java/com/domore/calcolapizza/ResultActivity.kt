6503ed8eeba3cc805a2fd354454bfac93ca4e2eb
Domenico Moreschini <tech593@yoox.com>
2018-04-13 17:54:58 +0200

    Wiring up results

--- app/src/main/java/com/domore/calcolapizza/ResultActivity.kt
+++ app/src/main/java/com/domore/calcolapizza/ResultActivity.kt
@@ -3,13 +3,22 @@ package com.domore.calcolapizza
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.serialization.json.JSON
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        PizzaCalculator.Calculate(JSON.parse(intent.getStringExtra("INPUT_DATA")))
+        val result = PizzaCalculator.Calculate(JSON.parse(intent.getStringExtra("INPUT_DATA")))
+
+        txt_acqua.text = result.acqua.toString()
+        txt_farina.text = result.farina.toString()
+        txt_lievito.text = result.lievito.toString()
+        txt_olio.text = result.grassi.toString()
+        txt_sale.text = result.sale.toString()
+        txt_prd.text = result.pdr.toString()
+
 
     }
 }
