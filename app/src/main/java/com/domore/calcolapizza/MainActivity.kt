23d1a64618ed309df98456dcf3bd2f7fe5b162fb
Domenico Moreschini <tech593@yoox.com>
2018-04-18 08:58:59 +0200

    minor refactor

--- app/src/main/java/com/domore/calcolapizza/MainActivity.kt
+++ app/src/main/java/com/domore/calcolapizza/MainActivity.kt
@@ -13,38 +13,47 @@ import kotlinx.serialization.json.JSON
 class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnFocusChangeListener {
 
     private var textBoxes : List<EditText> = emptyList()
+    private val isDebug = true
 
-    override fun onFocusChange(view: View?, hasFocus: Boolean) {
-        if(!(view is EditText) || hasFocus)
-            return
-        validateValues()
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    private fun validateEditText(editText: EditText){
-        val textValues = textBoxes.associateBy ( {it.id}, {it.text.toString()} )
-        val nw = textValues
-                .plus(Pair(switchTeglia.id, switchTeglia.isChecked.toString()))
-                .plus(Pair(pdr_types.id, pdr_types.selectedItemPosition.toString()))
-
-        if(editText.text.toString().isBlank() ) {
-            editText.error = "Il campo non può essere vuoto"
-            return
+        if(isDebug)
+            initializeEditTexts()
+        textBoxes = listOf(panielli, pesoPanielli, idro, sale, oreLievitazione, frigo, temperatura, grassi, pdr)
+        textBoxes.forEach { it.onFocusChangeListener = this }
+        btn_calcola.setOnClickListener(this)
+        switchTeglia.setOnCheckedChangeListener { _, _ -> validateAllInputs() }
+        pdr_types.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                validateAllInputs()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
         }
-        val error = InputValidatorFactory.GetFor(editText.id, nw)(editText.text.toString())
-        if(error != "")
-            editText.error = error
+        val adapter = ArrayAdapter.createFromResource(this, R.array.pdr_array, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        pdr_types.adapter = adapter
     }
 
-    private fun validateValues(){
-        textBoxes.forEach { validateEditText(it) }
+    private fun initializeEditTexts(){
+        panielli.setText("1")
+        pesoPanielli.setText("200")
+        idro.setText("65")
+        sale.setText("50")
+        oreLievitazione.setText("24")
+        frigo.setText("0")
+        temperatura.setText("20")
+        grassi.setText("0")
+        pdr.setText("0")
     }
 
     override fun onClick(view: View?) {
         val intent = Intent(this, ResultActivity::class.java)
-        validateValues()
-        val valuesWithErrors = textBoxes.filter { it.error != null && !it.error.isBlank() }
+        validateAllInputs()
 
-        if(valuesWithErrors.isEmpty()) {
+        if(textBoxes.all { it.error == null || it.error.isBlank() }) {
             val data = JSON.stringify(InputData(
                     panielli.text.toString().toInt(),
                     pesoPanielli.text.toString().toFloat(),
@@ -62,44 +71,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnFocusChan
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        btn_calcola.setOnClickListener(this)
-
-        panielli.onFocusChangeListener = this
-        panielli.setText("1")
-        pesoPanielli.onFocusChangeListener = this
-        pesoPanielli.setText("200")
-        idro.onFocusChangeListener = this
-        idro.setText("65")
-        sale.onFocusChangeListener = this
-        sale.setText("50")
-        oreLievitazione.onFocusChangeListener = this
-        oreLievitazione.setText("24")
-        frigo.onFocusChangeListener = this
-        frigo.setText("0")
-        temperatura.onFocusChangeListener = this
-        temperatura.setText("20")
-        grassi.onFocusChangeListener = this
-        grassi.setText("0")
-        pdr.onFocusChangeListener = this
-        pdr.setText("0")
-        switchTeglia.setOnCheckedChangeListener { _, _ -> validateValues() }
-        pdr_types.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                validateValues()
-            }
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-        }
-        val adapter = ArrayAdapter.createFromResource(this, R.array.pdr_array, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        pdr_types.adapter = adapter
-
-        textBoxes = listOf(panielli, pesoPanielli, idro, sale, oreLievitazione, frigo, temperatura, grassi, pdr)
+    override fun onFocusChange(view: View?, hasFocus: Boolean) {
+        if(!(view is EditText) || hasFocus)
+            return
+        validateAllInputs()
     }
+
+    private fun validateAllInputs(){
+        textBoxes.forEach { validateEditText(it) }
+    }
+
+    private fun validateEditText(editText: EditText){
+
+        val valuesMap = textBoxes
+                .associateBy( {it.id}, {it.text.toString()} )
+                .plus(Pair(switchTeglia.id, switchTeglia.isChecked.toString()))
+                .plus(Pair(pdr_types.id, pdr_types.selectedItemPosition.toString()))
+
+        if(editText.text.toString().isBlank() ) {
+            editText.error = "Il campo non può essere vuoto"
+            return
+        }
+        val validationFunction = InputValidatorFactory.GetFor(editText.id, valuesMap)
+        val error = validationFunction(editText.text.toString())
+        if(error != "")
+            editText.error = error
+    }
+
+
 
 
 }
